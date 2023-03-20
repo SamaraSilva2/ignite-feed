9821d8e3c3a16590035e8d62c4789e1a802779cf
SamaraSilva2 <sam.silva.oliveira@gmail.com>
2023-03-19 21:56:03 -0300

    Alterações

--- src/App.jsx
+++ src/App.jsx
@@ -21,12 +21,12 @@ const posts = [
     content: [
       { type: "paragraph", content: "Fala galeraa 👋" },
       { type: "paragraph", content: "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀" },
-      { type: "link", content: "liana.design/doctorcare" },              
+      { type: "link", content: "liana.design/doctorcare" },
     ],
     publishedAt: new Date('2022-05-03 20:00'),
   },
   {
-    id: 1,
+    id: 2,
     author: {
       avatarUrl: "https://this-person-does-not-exist.com/gen/avatar-11c3f3091ccf342b5c9b7f13eb680d6e.jpg",
       name: "Nuno Seixas",
@@ -35,7 +35,7 @@ const posts = [
     content: [
       { type: "paragraph", content: "Olá, pessoal!" },
       { type: "paragraph", content: "Hoje fiz o desenho funcional do Back-End do nosso próximo projeto prático ⚡" },
-      { type: "link", content: "seixas.dev/projetos" },              
+      { type: "link", content: "seixas.dev/projetos" },
     ],
     publishedAt: new Date('2022-05-07 20:00'),
   }
@@ -49,14 +49,14 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          { posts.map(post => {
+          {posts.map(post => {
             return (
-              <Post 
+              <Post
                 key={post.id}
                 author={post.author}
                 content={post.content}
                 publishedAt={post.publishedAt}
-                />  
+              />
             )
           })
           }
--- src/componentes/Post.jsx
+++ src/componentes/Post.jsx
@@ -48,36 +48,37 @@ export function Post({ author, publishedAt, content }) {
                 </div>
 
                 <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
-                {publishedDateRelativeToNow}
+                    {publishedDateRelativeToNow}
                 </time>
             </header>
 
             <div className={styles.content}>
                 {content.map(line => {
-                    if(line.type === "paragraph") {
-                      return <p>{line.content}</p>;
-                    } else if(line.type === "link") {
-                        return <p><a href="#">{line.content}</a></p>
-                        }
+                    if (line.type === "paragraph") {
+                        return <p key={line.content}>{line.content}</p>;
+                    } else if (line.type === "link") {
+                        return <p key={line.content}><a href="#">{line.content}</a></p>
                     }
+                }
                 )}
             </div>
             <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <strong>Deixe seu feedback</strong>
-                
-                <textarea 
+
+                <textarea
                     name="comment"
                     placeholder="Deixe um comentário"
+                    value={newCommentText}
                     onChange={handleNewCommentChange}
                 />
                 <footer>
                     <button type="submit">Publicar</button>
                 </footer>
-                
+
             </form>
             <div className={styles.commentList}>
                 {comments.map(comment => {
-                    return <Comment key={comment} content={comment}/>
+                    return <Comment key={comment} content={comment} />
                 })}
             </div>
 
